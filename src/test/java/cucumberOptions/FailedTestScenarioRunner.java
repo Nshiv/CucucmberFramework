d879ead034a6e6df6aef76f3e9165b3f24205b9c
Nshiv <kshiv1105@gmail.com>
2023-07-04 23:59:16 +0530

    Enhancements on extent reporting and Failed scenarios

--- src/test/java/cucumberOptions/FailedTestScenarioRunner.java
+++ src/test/java/cucumberOptions/FailedTestScenarioRunner.java
@@ -5,13 +5,13 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(glue = "stepDefinitions",
-        features = "src/test/java/features",
+        features = "@target/failed_scenarios.txt",
         monochrome = true,
         tags = "@Regression",
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","rerun:target/failed_scenarios.txt"}
 
 )
-public class Parrallaexecution extends AbstractTestNGCucumberTests
+public class FailedTestScenarioRunner extends AbstractTestNGCucumberTests
 {
     @Override
     @DataProvider(parallel = true)
--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,7 +1,14 @@
 package stepDefinitions;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Scenario;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import utilities.ContextSetUp;
+
+import java.io.File;
 import java.io.IOException;
 public class Hooks
 {
@@ -28,5 +35,17 @@ public class Hooks
         {
             driver.close();
         }
+    }
+
+    @AfterStep
+    public void addScreenShot(Scenario scenario) throws IOException {
+        if(scenario.isFailed())
+        {
+            WebDriver driver =contextSetUp.testBase.WebDriverManager();
+            File sourcePath=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            byte[] filecontent = FileUtils.readFileToByteArray(sourcePath);
+            scenario.attach(filecontent,"image/png","image");
+        }
+
     }
 }
