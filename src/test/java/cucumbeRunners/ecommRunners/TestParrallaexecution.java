43d66005db53ab196ecaf74a547b1422ca23f08d
Nshiv <kshiv1105@gmail.com>
2023-08-05 18:56:07 +0530

    few more tweaks

--- src/test/java/cucumbeRunners/ecommRunners/TestParrallaexecution.java
+++ src/test/java/cucumbeRunners/ecommRunners/TestParrallaexecution.java
@@ -24,3 +24,23 @@ public class TestParrallaexecution extends AbstractTestNGCucumberTests
 
 }
 */
+
+
+package cucumbeRunners.ecommRunners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        glue = {"stepDefinitions/practiceWebStepDefinition","cucumberHooks"},
+        features = "src/test/java/features/practiceWebsite",
+        monochrome = true,
+        tags="@smoke",
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","rerun:target/failed_scenarios.txt"}
+)
+
+public class TestParrallaexecution extends AbstractTestNGCucumberTests
+{
+
+
+}
